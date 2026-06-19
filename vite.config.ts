ffd900be7acbbcb91cb5da7dbc2956f68567d702
Manoj Rathi <themanojrathi@gmail.com>
2026-06-19 21:53:33 +0530

    feat: add base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/google-ai-studio-app/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
